779605b1efe09fa86f56eaa28f24b3d9dac7cfdc
gombrowicz <gombrowicz.gombrowicz@wp.pl>
2018-09-09 12:23:34 +0200

    intelliJ stuff: shortcuts and plugins

---
+++ intelliJ/intelliJPlugins.java
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+		# I N T E L L I J   P L U G I N S
+		# I N T E L L I J   P L U G I N S
+		# I N T E L L I J   P L U G I N S
+		# I N T E L L I J   P L U G I N S
+		# I N T E L L I J   P L U G I N S
+		# I N T E L L I J   P L U G I N S
+		# I N T E L L I J   P L U G I N S
+		
+		
+		
+			(A) jak instalować
+		
+
+			(0) Grep Console - customizacja outputu consolowego - kolory, tła podług regexów i innych
+		
+		
+		
+		
+		
+		
+		
+		
+			
+#(A)======== jak instalować
+	/** File >> Settings > Plugins >> [Grep Console ściągnąłem ze stronki w postaci .zip, tutaj wybrałem 'Install plugin from disk....']
+		>> po restarcie wszystko działało*/
---
+++ intelliJ/intelliJShortcuts.java
@@ -0,0 +1,117 @@
+
+
+
+			
+			
+
+
+		# I N T E L L I J   S H O R T C U T S
+		# I N T E L L I J   S H O R T C U T S
+		# I N T E L L I J   S H O R T C U T S
+		# I N T E L L I J   S H O R T C U T S
+		# I N T E L L I J   S H O R T C U T S
+				
+				
+https://resources.jetbrains.com/storage/products/intellij-idea/docs/IntelliJIDEA_ReferenceCard.pdf				
+				
+				# bookmark
+				'ctrl + shift + 0-9'	- creates a bookmark in carret line in file, one can jump to this place from everywhere using 'ctrl + n'
+
+				# switch between files/windows
+				'ctrl + tab'	- can jump from terminal to class etc.
+
+				# scratch file
+				'ctrl + shift + alt + insert'	- create a scratch file
+				// let say we've got a file someYml.yml, if we select some part of this file and then hit shortcut that creates a scratch file then that file will comprise selected text and will inherit extension from oryginal file 
+				
+				# pokaż, gdzie ostatnio dokonano zmian
+				'ctrl + shift + backspace' - można się cofać wiele razy
+
+				# wstaw breakpointa
+				'ctrl + F8' - stwórz breakpointa w linijce z carretem	
+
+				# otwórz w edytorze dowolny plik z projektu
+				'ctrl + shift + N' - wpisz w szukaczce diry/+ nazwę pliku
+				
+				# side scroll
+				'shift + scroll' - wciśnięty shift pozwala scrollować ekran w poziomie
+				
+				# "replay" - executes code previously executed
+				'ctrl + F5'
+				
+				# FIND USAGES
+				'ctrl + shift + F7', naviagete beetwen usages using 'F3', escape searching mode by hitting 'esc'
+				
+				# GO TO DECLARATION
+				'ctrl + B' - naviagates from method call to method declaration
+				
+				# CREATE VARIABLE
+				'someStatementProducingVariable' + '.var + enter'
+				
+				# GENERATE VARIABLE NAME based on instance type
+				'private FileOutputStream' + 'ctrl + space' will result in 'private FileOutputStream fileOutputStream'
+				
+				# GENERATE CODE
+				'alt + insert + fn (ONM)'
+				
+				# MOVE BETWEEN TABS
+				'alt + left/right'
+					
+				# MOVE BETWEEN METHODS
+				'alt + up/down'
+				
+				# CREATE VARIABLE
+				'ctrl + alt + V'
+				creates local variable from given syntax
+				e.g. Arrays.asList("Czarna Mamba"); plus 'ctrl + alt + V' = List<String> strings = Arrays.asList("Czarna Mamba");
+
+				# HOW INHERITANE HIERARCHY
+				'ctrl + H' 
+				opens up a pane with inheritance hierarchy
+							
+				# RENAME			
+				'shift + F6'
+				rename 
+				
+				# MOVE TEXT
+				'ctrl + SHIFT + up/down'
+				moves line up/down
+				
+				# OVERRIDE 	METHODS
+				'ctrl + O'
+
+				# IMPLEMENT METHODS
+				'ctrl + I'
+				
+				# SELECT VIEW OF PROJECT/FILE/METHODS
+				'alt + 1'
+				
+				# FIND IMPLEMENTATION OF ABSTRACT METHOD
+				'ctrl + alt + B'
+				To navigate to the implementation(s) of an abstract method, position the caret at its usage or its name in the declaration and press Ctrl+Alt+B.
+				
+				# FIND USAGES of methods
+				'alt + F7'
+				
+				# CASTING HELP
+				String s = (carret is here + 'ctrl + shift + space' => will result in String s = (String)
+				
+				# INNER CLASS as OUTER CLASS in the same file or in separate file
+				'F6' when carret is placed on inner class declaration - menu opens up and one can pick how to refactor inner class
+				
+				# NAVIGATE THROUGH CHANGES IN CODE
+				'ctrl + alt + C'
+				
+				# QUICK FIX MENU POP UP
+				'alt + enter' - caret has to be within problematic code
+				
+				# SURROUND LINE OF CODE WITH - pop up pane with options such as: try/catch or if statement OR while loop 
+				'ctrl + alt + T' - caret properly placed
+				
+				# SOURROUND AUTOCLOSBLE INSTANCE WITH TRY WITH RESOURCES
+				'alt + enter' and pick "surround with try with resources"
+				
+				# BUILD IN REGEX CHECKER
+				'alt + enter' - caret on regex, pick check regex
+				
+				
